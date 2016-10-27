1bd4bebf066a3dd843e6ba262d2995faf90eea33
Hugh Dickinson <hdickins@umn.edu>
2016-10-27 17:06:46 -0500

    Revert "Minor update"
    This reverts commit 4927560ef24b57dab2d8019887751f7b67a01369.

--- DCWAggregationTest/serveConsensus.php
+++ DCWAggregationTest/serveConsensus.php
@@ -344,7 +344,7 @@ class ReliabilitySamplePrinter{
 }
 
 // INITIAL DATABASE CONNECTION
-$database = new mysqli('localhost', 'REDACTED', 'REDACTED', 'dcwConsensus');
+$database = new mysqli('localhost', 'root', '!Ocus1!Ocus1', 'dcwConsensus');
 
 if($database->connect_errno > 0){
   die('Unable to connect to database [' . $database->connect_error . ']');
